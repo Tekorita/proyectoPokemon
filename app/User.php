277c1a6d66fe401b6744a7cbb2cf572bac9f2802
David Palma <davidpalmalugo@gmail.com>
2019-03-29 18:14:02 -0300

    desarrollo de validaciones de roles

--- app/User.php
+++ app/User.php
@@ -13,6 +13,37 @@ class User extends Authenticatable
     public function roles(){
         return $this->belongsToMany('proyectoPokemon\Role');//relacion muchos a muchos
     }
+
+    //esta validacion es para autorizar el pase de usuario luego de verificar si tiene algun role
+    public function authorizeRoles($roles){
+        if($this->hasAnyRole($roles)){
+            return true;
+        }
+        abort(401,'This action is unauthorized');
+    }
+
+    //esta validacion verifica si tiene varios arreglos osea si es un arreglo o no lo es cuando tienen un solo rol
+    public function hasAnyRole($role){
+        if(is_array($roles)){
+            foreach ($roles as $role) {
+                if($this->hasRole($role)){
+                    return true;
+                }
+            }
+        } else {
+            if($this->hasRole($roles)){
+                return true;
+            }
+        }
+    }
+
+    //Esta validacion es para verificar que un usario tenga un rol
+    public function hasRole($role){ 
+        if($this->roles()->where('name',$role)->first()){ 
+            return true;
+        }
+        return false;
+    }
     /**
      * The attributes that are mass assignable.
      *
