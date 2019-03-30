860b524d5f122a8b192253afd0b647f538814034
David Palma <davidpalmalugo@gmail.com>
2019-03-30 05:02:51 -0300

    se creo el modelo de pokemones y sus migraciones

--- app/Http/Controllers/PokemonController.php
+++ app/Http/Controllers/PokemonController.php
@@ -12,8 +12,20 @@ class PokemonController extends Controller
                 ['id' => 1, 'name' => 'Picachu'],
                 ['id' => 2, 'name' => 'Squirtle'],
                 ['id' => 3, 'name' => 'Charizard'],
-            ]);
+            ], 200); // 200 xq es una respuesta satisfactoria
         }
         return view('pokemons.index');
+    }
+
+    public function store(Request $request){
+        if($request->ajax()){
+            $pokemon = new Pokemon();
+            $pokemon->name = $request->input('name');
+            $pokemon->picture = $request->input('picture');
+            $pokemon->save();
+            return response()->json([
+                "message" => "Pokemon creado correctamente" //segun json api donde creamos microservicios tenemos que responder http cuando la respuesta es satisfactoria se responde con el numero 200
+            ], 200);
+        }
     }
 }
